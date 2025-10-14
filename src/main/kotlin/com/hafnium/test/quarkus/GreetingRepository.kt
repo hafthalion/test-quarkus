523dbdb57f90019b81ff31843d20484963d43e95
Jaroslav Abraham <jabraham1979@gmail.com>
2025-10-14 13:32:27 +0200

    Refactor database repository

--- src/main/kotlin/com/hafnium/test/quarkus/GreetingRepository.kt
+++ src/main/kotlin/com/hafnium/test/quarkus/GreetingRepository.kt
@@ -1,8 +1,8 @@
 package com.hafnium.test.quarkus
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GreetingRepository : CrudRepository<GreetingEntity, Int> {
+interface GreetingRepository : JpaRepository<GreetingEntity, Int> {
 }
